77b496b19088b374e0b8bfd6560eb505592a10e6
adiAlicecode <adiAlicecode@gmail.com>
2021-01-16 13:58:31 +0200

    change cards

--- src/component/Bucharim.js
+++ src/component/Bucharim.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Bucharim.css';
-import { Card, Image,Button } from 'semantic-ui-react'
 import background2 from '../pictures/background2.jpg';
 import Dushpere from '../pictures/dushpere.jpeg';
 import GrapeLeaves from '../pictures/GrapeLeaves.jpg';
@@ -23,54 +22,54 @@ class Bucharim extends Component{
                 <img className="background2" src={background2} alt="background2"/>
                 <div className="headTitle">מאכלים בוכריים</div>
                 <div className="startCards">
-                    <Card style={{marginTop:20,height:320}}>
-                        <Image src={Dushpere} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>דושפרה</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20}}>
-                        <Image src={GrapeLeaves} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>עלי גפן</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={SoupUro} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>מרק אורו</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={Bahsh} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>באחש</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={Mantu} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>מנטו</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={Osvo} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>אוסבו</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={Burecas} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>בורקסים</Button>
-                        </Card.Content>
-                    </Card>
-                    <Card style={{marginTop:20,height:325}}>
-                        <Image src={Kovroran} wrapped ui={false} style={{height:255}}/>
-                        <Card.Content >
-                        <Button fluid inverted color='red'>קוורורן</Button>
-                        </Card.Content>
-                    </Card>
+                    <p className="btnDushpere">
+                        <span>
+                            <img src={Dushpere} className="img1"/>
+                        </span>
+                    <button className="btnAll">דושפרה</button> 
+                    </p>
+                    <p className="btnGrapeLeaves">
+                        <span>
+                            <img src={GrapeLeaves} className="img1"/>
+                        </span>
+                    <button className="btnAll">עלי גפן</button> 
+                    </p>
+                    <p className="btnSoupUro">
+                        <span>
+                            <img src={SoupUro} className="img1"/>
+                        </span>
+                    <button className="btnAll">מרק אורו</button> 
+                    </p>
+                    <p className="btnBahsh">
+                        <span>
+                            <img src={Bahsh} className="img1"/>
+                        </span>
+                    <button className="btnAll">באחש</button> 
+                    </p>
+                    <p className="btnMantu">
+                        <span>
+                            <img src={Mantu} className="img1"/>
+                        </span>
+                    <button className="btnAll">מנטו</button> 
+                    </p>
+                    <p className="btnOsvo">
+                        <span>
+                            <img src={Osvo} className="img1"/>
+                        </span>
+                    <button className="btnAll">אוסבו</button> 
+                    </p>
+                    <p className="btnBurecas">
+                        <span>
+                            <img src={Burecas} className="img1"/>
+                        </span>
+                    <button className="btnAll">בורקסים</button> 
+                    </p>
+                    <p className="btnKovroran">
+                        <span>
+                            <img src={Kovroran} className="img1"/>
+                        </span>
+                    <button className="btnAll">קוורורן</button> 
+                    </p>
                 </div>
             </div>
         )
--- src/component/Cookies.js
+++ src/component/Cookies.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import cookiesImg from '../pictures/cookies.jpg'
+import cookiesImg from '../pictures/cookies.jpg';
+import {Button} from 'semantic-ui-react';
 import './pictures.css';
+import './cookies.css';
+import Shosanim from '../pictures/shosanim.jpeg';
 
 class Cookies extends Component{
     render(){
@@ -8,6 +11,14 @@ class Cookies extends Component{
             <div>
                 <img className="cookiesImg" src={cookiesImg} alt="cookiesImg"/>
                 <div className="headTitle">עוגיות</div>
+                <div className="startCards">
+                    <p className="btnShosanim">
+                        <span>
+                            <img src={Shosanim} className="img1"/>
+                        </span>
+                    <button className="btnAll">שושנים</button> 
+                    </p>
+                </div>
             </div>
         )
     }
